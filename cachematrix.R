6b69feef22c313a6903abe6303869e404e888a71
Dan Williams <Dan_Williams2@nfumutual.co.uk>
2018-02-25 11:34:24 +0000

    cache inverse matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL ##sets m to null internal to function and assigned default value
+    set<-function(y){
+      x<<-y ##sets x to y in/outside of current environment so cache solve can see if data exists
+      m<<-NULL ##sets m to null both in/outside of current environment
+    }
+    
+    get<-function() x 
+    setmatrix<-function(solve) 
+      m<<-solve 
+    getmatrix<-function() m 
+    
+    list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix) ##list containing functions
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatrix()
+  ##checks to see whether m is null as condition for if function
+  if(!is.null(m)) {
+    message("Pulled from cache") ##informs user data is returned from cache when m is not NULL
+    return(m)
+  }
+  
+  matrix<- x$get()
+  m<-solve(matrix,...) ##assigns return of solve function to m which is the inverse figures 
+  x$setmatrix(m) ##sets cached data
+  m        ## Return a matrix that is the inverse of 'x'
 }
